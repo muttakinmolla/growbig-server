4d68de7a74b9e5005b006b120327212cac4149c3
muttakin1991 <shawonmuttakin@gmail.com>
2022-10-25 20:20:45 +0600

    course details api done

--- index.js
+++ index.js
@@ -7,13 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/mainCourses/MainCourses.json');
+const allCourses = require('./data/AllCourse/AllCourses.json');
 
-app.get('/', (req, res) => {
+app.get('/news', (req, res) => {
     res.send(courses)
 })
 
-app.get('/courses/:id', (req, res) => {
-    res.send(req.params.id)
+app.get('/courses', (req, res) => {
+    res.send(allCourses)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selected_course = allCourses.filter(course => course.course_id == id);
+    res.send(selected_course)
+})
+
+app.get('/course-details/:id', (req, res) => {
+    const id = req.params.id;
+    const course_details = allCourses.find(course => course._id == id);
+    res.send(course_details);
 })
 
 app.listen(port, () => {
